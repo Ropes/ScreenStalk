626ae036361442dfce9e7f6ba823c1e9cb283ead
Josh Roppo <joshroppo@gmail.com>
2012-02-10 01:18:11 -0800

    First commit

---
+++ screenstalk.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+hash xwd 2>&-
+if [ $? -eq 0 ]
+then
+  cmd='xwd -root | convert xwd:-'
+else
+  echo "xwd not installed on system"
+  exit 0
+fi
+
+echo -n "Enter number of minutes between screen captures:"
+read nap
+
+ne=$(( nap * 5 ))
+
+echo -n "Enter filename:"
+read name
+
+save_dir="$HOME/screenstalk"
+
+if [ ! -d $save_dir ]
+then
+  mkdir $save_dir 
+fi
+
+i=0
+while true
+do
+  time=`date +%H-%M-%S`
+
+  filename="$name$i--$time.png" 
+  echo "$time $i"
+  screen="$save_dir/$filename" 
+
+  echo "$cmd $screen"
+
+  sleep $ne 
+  i=$(( $i + 1 ))
+done
